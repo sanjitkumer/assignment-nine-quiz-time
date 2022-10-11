93e0d2a4a6bcfa538f2e4016565a4d9f45418e3f
sanjitkumer <sanjitk666@gmail.com>
2022-10-11 13:55:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2 className='text-4xl font-bold'>This is header</h2>
+    </div>
+  );
+}
+
+export default App;
