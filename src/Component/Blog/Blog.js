05c4ab66d80730dd0820f2f1c92968dd62f9afb6
sanjitkumer <sanjitk666@gmail.com>
2022-10-12 10:47:10 +0600

    added frist question answer

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -5,7 +5,7 @@ const Blog = () => {
         <div>
            <div>
             <h2>What is a purpose of react router?</h2>
-            <p>Ans:</p>
+            <p>Ans:React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
            </div>
            <div>
             <h2>How does context API work?</h2>
