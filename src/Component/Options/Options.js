a3d585f3660dffef57cc6741994dfeba7b0e46ba
sanjitkumer <sanjitk666@gmail.com>
2022-10-13 22:47:40 +0600

    added tostify

--- src/Component/Options/Options.js
+++ src/Component/Options/Options.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Options = ({option}) => {
+const Options = ({option,cheekAns}) => {
     
     return (
-        <div className='bg-red-200 text-center p-4'>
-            <p className='m-2 p-4 bg-green-300 text-2xl'>{option}</p>
+        <div  onClick={()=> cheekAns(option)} className='cursor-pointer bg-red-200 text-center m-2 p-4'>
+            <p>{option}</p>
         </div>
     );
 };
--- src/Component/Quizquestion.js
+++ src/Component/Quizquestion.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import Options from './Options/Options';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
   
 
 const Quizquestion = ({questions}) => {
     const [show,setShow]= useState(true)
+
+    const cheekAns = (ans)=> {
+        if (ans === questions.correctAnswer){
+            toast("Answer is correct!!");
+        }
+        else{
+            toast("Answer is not correct");
+        }
+
+    }
     return (
         <div className='bg-slate-100 w-9/12 mx-auto mt-4 p-6'>
             <h1 className='text-2xl m-4'>{questions.question}</h1>
-            {questions.options.map((option,index) => <Options key = {index} option={option}></Options>)}
+          <div>
+          {questions.options.map((option,index) => <Options key = {index} option={option} cheekAns = {cheekAns} ></Options>)}
+          <ToastContainer />    
+          </div>
 
         <div onClick={()=> setShow(!show)} >
             {show ? <p><FontAwesomeIcon icon={faEye}></FontAwesomeIcon> </p> : <p><FontAwesomeIcon icon={faEyeSlash}></FontAwesomeIcon></p>}
